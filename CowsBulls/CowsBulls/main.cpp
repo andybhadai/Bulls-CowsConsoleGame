298bc3195828fd2bd6f04cf2ffb84ed53015a74b
Andy Bhadai <abhadai@gmail.com>
2017-04-15 18:25:04 +0200

    Cleaned up

--- CowsBulls/CowsBulls/main.cpp
+++ CowsBulls/CowsBulls/main.cpp
@@ -3,32 +3,59 @@
 
 using namespace std;
 
+void IntroduceGame();
+void PlayGame();
+bool AskToPlayAgain();
+void PrintToScreen(string message);
+string GetAndReturnInput();
+
 // Sample function
 int doubleMe(int x) {
 	return x * 2;
 }
+int main() {
 
-// Introduce the game
-void printToScreen(string message) {
+	IntroduceGame();
+
+	do {
+		PrintToScreen("Playing the game!");
+		PlayGame();
+	} while (AskToPlayAgain());
+
+	return 0;
+}
+
+// Introduce player to the game
+void IntroduceGame() {
+	PrintToScreen("Can you guess the 9-letter word?");
+	PrintToScreen("Enter you guess: ");
+}
+
+void PlayGame() {
+	constexpr int MAX_TRIES = 5;
+
+	for (int i = 1; i <= MAX_TRIES; i++)
+		cout << "Your guess was " << GetAndReturnInput() << endl;
+}
+
+bool AskToPlayAgain()
+{
+	PrintToScreen("Maximum tries reached! Do you want to play again?");
+	string response = "";
+	getline(cin, response);
+
+	return (response[0] == 'y' || response[0] == 'Y');
+}
+
+// Print something on the screen
+void PrintToScreen(string message) {
 	cout << message << endl;
 }
 
 // Return player input
-string getAndReturnInput() {
+string GetAndReturnInput() {
+	PrintToScreen("Enter your guess: ");
 	string guess = "";
 	getline(cin, guess);
 	return guess;
-}
-
-int main() {
-
-	// Define constants
-	//constexpr int WORD_COUNT = 9;
-
-	printToScreen("Can you guess the 9-letter word?");
-	printToScreen("Enter you guess: ");
-		
-	cout << "Your guess was: " << getAndReturnInput() << endl;
-
-	return 0;
 }
